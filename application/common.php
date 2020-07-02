a4f5cd8e83bb51c449a54e3404783a1729f3d908
chunchunchundashui <49420740+chunchunchundashui@users.noreply.github.com>
2020-07-02 10:44:45 +0800

    Update common.php

--- application/common.php
+++ application/common.php
@@ -31,12 +31,12 @@ function sendEmail($data = []) {
     $mail->CharSet = "UTF-8"; //字符集
     $mail->Encoding = "base64"; //编码方式
 
-    $mail->Username = 'changchunsinger@126.com';  //你的邮箱
-    $mail->Password = 'ycc15082730141';  //你的密码
+    $mail->Username = '';  //你的邮箱
+    $mail->Password = '';  //你的密码
     $mail->Subject = "注册管理员账户成功!"; //邮件标题
 
-    $mail->From = 'changchunsinger@126.com';  //发件人地址（也就是你的邮箱）
-    $mail->FromName = '长春个人博客-注册账号';  //发件人姓名
+    $mail->From = '';  //发件人地址（也就是你的邮箱）
+    $mail->FromName = '个人博客-注册账号';  //发件人姓名
 
     if($data && is_array($data)){
         foreach ($data as $k=>$v){
@@ -62,13 +62,13 @@ function sendEmail($data = []) {
          $mail->CharSet = "utf8";// 编码格式为utf8，不设置编码的话，中文会出现乱码
          $mail->Host = "smtp.163.com";// 发送方的SMTP服务器地址
          $mail->SMTPAuth = true;// 是否使用身份验证
-         $mail->Username = "chunchunsinger@163.com";// 发送方的163邮箱用户名，就是你申请163的SMTP服务使用的163邮箱</span><span style="color:#333333;">
-         $mail->Password = 'NPCSQEFRJRDMOTBA';// 发送方的邮箱密码，注意用163邮箱这里填写的是“客户端授权密码”而不是邮箱的登录密码！</span><span style="color:#333333;">
+         $mail->Username = "";// 发送方的163邮箱用户名，就是你申请163的SMTP服务使用的163邮箱</span><span style="color:#333333;">
+         $mail->Password = '';// 发送方的邮箱密码，注意用163邮箱这里填写的是“客户端授权密码”而不是邮箱的登录密码！</span><span style="color:#333333;">
          $mail->SMTPSecure = "ssl";// 使用ssl协议方式</span><span style="color:#333333;">
          $mail->Port = 994;// 163邮箱的ssl协议方式端口号是465/994
-         $mail->setFrom("chunchunsinger@163.com","作者");// 设置发件人信息，如邮件格式说明中的发件人，这里会显示为Mailer(xxxx@163.com），Mailer是当做名字显示
+         $mail->setFrom("","作者");// 设置发件人信息，如邮件格式说明中的发件人，这里会显示为Mailer(xxxx@163.com），Mailer是当做名字显示
          $mail->addAddress($toemail,'博客回复消息');// 设置收件人信息，如邮件格式说明中的收件人，这里会显示为Liang(yyyy@163.com)
-         $mail->addReplyTo("chunchunsinger@163.com","春春春");// 设置回复人信息，指的是收件人收到邮件后，如果要回复，回复邮件将发送到的邮箱地址
+         $mail->addReplyTo("","");// 设置回复人信息，指的是收件人收到邮件后，如果要回复，回复邮件将发送到的邮箱地址
          //$mail->addCC("xxx@163.com");// 设置邮件抄送人，可以只写地址，上述的设置也可以只写地址(这个人也能收到邮件)
          //$mail->addBCC("xxx@163.com");// 设置秘密抄送人(这个人也能收到邮件)
          //$mail->addAttachment("bug0.jpg");// 添加附件
